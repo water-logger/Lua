ea9ee71c69eea803e7325cf984cb6f6ddd4f2c1c
_water <101909986+water-logger@users.noreply.github.com>
2023-04-30 21:04:56 -0500

    stupid fuckin cloud
    You should kill your self now! *Lightning*

--- r6-Scripts/Rain Cloud.lua
+++ r6-Scripts/Rain Cloud.lua
@@ -9,10 +9,12 @@ part.Parent = workspace
 part.Size = Vector3.new(5, 5, 5)
 part.Color = Color3.new(1, 1, 1)
 part.Anchored = true
+part.Name = "stupid cloud"
 
 mesh.Parent = part
 mesh.Scale = Vector3.new(4, 2, 4)
 mesh.MeshId = "rbxassetid://11328480674"
+mesh.Name = "i am the storm that is approaching, provoking, black clouns in isolation. I am reclaimer of my name, born in flame. I have been blessed, my family crest is a demon of death!"
 
 ParticleEmitter.LightInfluence = 1
 ParticleEmitter.Lifetime = NumberRange.new(1, 1)
@@ -21,6 +23,7 @@ ParticleEmitter.Rate = 1000
 ParticleEmitter.EmissionDirection = Enum.NormalId.Bottom
 ParticleEmitter.Texture = "rbxassetid://671728795"
 ParticleEmitter.Parent = part
+ParticleEmitter.Name = "rainwaa"
 
 while true do
 	local player = game.Players.LocalPlayer
